9961443ff45be0e2e7e9950cbff3521c832c2f02
Frédéric Hardy <frederic.hardy@mageekbox.net>
2018-07-18 09:56:18 +0200

    Add missing unit test.

--- tests/units/src/net/uri/authority/user/info/user/any.php
+++ tests/units/src/net/uri/authority/user/info/user/any.php
@@ -3,6 +3,7 @@
 require __DIR__ . '/../../../../../../../runner.php';
 
 use norsys\score\tests\units;
+use mock\norsys\score as mockOfScore;
 
 class any extends units\test
 {
@@ -22,6 +23,26 @@ class any extends units\test
 
 			->exception(function() use (& $exception) { $this->newTestedInstance('', $exception = new \exception); })
 					->isIdenticalTo($exception)
+		;
+	}
+
+	function testRecipientOfStringIs()
+	{
+		$this
+			->given(
+				$this->newTestedInstance($string = uniqid()),
+				$recipient = new mockOfScore\php\string\recipient
+			)
+			->if(
+				$this->testedInstance->recipientOfStringIs($recipient)
+			)
+			->then
+				->object($this->testedInstance)
+					->isEqualTo($this->newTestedInstance($string))
+				->mock($recipient)
+					->receive('stringIs')
+						->withArguments($string)
+							->once
 		;
 	}
 }
